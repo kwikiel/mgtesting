2d39c0533cf994bf5e0163614bcad715f15aca12
Kacper Wikiel <random8902@gmail.com>
2016-01-05 00:13:22 +0100

    Parsing at will

--- getdata.py
+++ getdata.py
@@ -4,9 +4,13 @@ import datetime
 from lxml import html
 import requests
 
+from models import db
+
 raw = requests.get("https://loanbase.com/stats")
 tree = html.fromstring(raw.text)
 
-active_loans =  float(tree.xpath('//div[@class="statistic"]/p//text()')[2].split(" ")[1])
+active_loans = float(tree.xpath('//div[@class="statistic"]/p//text()')[2].split(" ")[1])
 
-
+m = Market(lbactive=active_loans, created=datetime.datetime.utcnow())
+db.session.add(m)
+db.session.commit()
